49643169d8b939cae3ab916a58369b42bbadc7dd
Robin Campos <magyk81@gmail.com>
2024-06-21 00:26:03 -0600

    Add footer

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -22,18 +22,6 @@ function getPageHeight() {
     );
 }
 
-// Import images from head block.
-const headerCanvas = document.getElementById("header_canvas"),
-      headerCtx = headerCanvas.getContext("2d");
-const titleArea = document.getElementsByClassName("title_area")[0],
-      downloadResumeButton = document.getElementById("download_resume_button");
-const headerImageGoat = document.getElementById("header_img_goat"),
-      headerImageGoatFlipped = document.getElementById("header_img_goat_flipped"),
-      headerImageRobin = document.getElementById("header_img_robin"),
-      headerImageRobinFlipped = document.getElementById("header_img_robin_flipped"),
-      headerImageFence = document.getElementById("header_img_fence"),
-      headerImageFenceFlipped = document.getElementById("header_img_fence_flipped");
-
 function drawImage(ctx, image, { x: xPos, y: yPos }, { width: rightBound, height: lowerBound }) {
     if (rightBound === 0 && lowerBound === 0) {
         console.error("Invalid arguments: rightBound or lowerBound must be non-zero.");
@@ -51,6 +39,19 @@ function drawImage(ctx, image, { x: xPos, y: yPos }, { width: rightBound, height
         return scaledHeight;
     }
 }
+
+const headerCanvas = document.getElementById("header_canvas"),
+      headerCtx = headerCanvas.getContext("2d");
+const titleArea = document.getElementsByClassName("title_area")[0],
+      downloadResumeButton = document.getElementById("download_resume_button");
+
+// Import images for head block.
+const headerImageGoat = document.getElementById("header_img_goat"),
+      headerImageGoatFlipped = document.getElementById("header_img_goat_flipped"),
+      headerImageRobin = document.getElementById("header_img_robin"),
+      headerImageRobinFlipped = document.getElementById("header_img_robin_flipped"),
+      headerImageFence = document.getElementById("header_img_fence"),
+      headerImageFenceFlipped = document.getElementById("header_img_fence_flipped");
 
 function drawHeader() {
     let titleAreaRect = titleArea.getBoundingClientRect(),
@@ -113,12 +114,66 @@ function drawHeader() {
             {width: 0, height: lowerBound}
         );
     }
+
+    // Set spacer to be the same height.
+    document.getElementById('header_spacer').setAttribute("style", "height: " + lowerBound + "px");
+
+    return lowerBound;
 }
 
+function DemoProject(name, shotCount) {
+    this.cardImage = document.getElementById("card_img_" + name);
+    this.cardShots = [];
+    for (let i = 1; i <= shotCount; i++) {
+        this.cardShots.push(document.getElementById("shot_img_" + name + "_" + i));
+    }
+}
+
+const demoProjects = [];
+
+function drawCards(upperBound, lowerBound) {
+    let dims = [getPageWidth(), lowerBound - upperBound];
+    console.log(dims);
+}
+
+const footerCanvas = document.getElementById("footer_canvas"),
+      footerCtx = footerCanvas.getContext("2d");
+const footerArea = document.getElementsByClassName("footer_area")[0];
+
+function drawFooter() {
+    let textAreaRect = footerArea.getBoundingClientRect(),
+        upperBound = textAreaRect.top;
+    
+    footerCanvas.width = getPageWidth();
+    footerCanvas.height = textAreaRect.height;
+
+    footerCtx.fillStyle = "gray";
+    footerCtx.fillRect(0, 0, footerCanvas.width, footerCanvas.height);
+
+    return upperBound;
+}
+
+var headerLowerBound, footerUpperBound;
+
 window.onload = function() {
-    drawHeader();
+    headerLowerBound = drawHeader();
+    footerUpperBound = drawFooter();
+
+    // Load project stuff.
+    demoProjects.push(
+        new DemoProject("cloudcast", 1),
+        new DemoProject("bokkusu", 3),
+        new DemoProject("wizzers", 1),
+        new DemoProject("rainfall", 0),
+        new DemoProject("droserogis", 2),
+        new DemoProject("dauntless", 3),
+    );
+
+    drawCards(headerLowerBound, footerUpperBound);
 }
 
 window.onresize = function() {
     drawHeader();
+    drawFooter();
+    drawCards(headerLowerBound, footerUpperBound);
 }
